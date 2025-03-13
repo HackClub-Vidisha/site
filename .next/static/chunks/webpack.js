ada720df19a12c78bbadfd1c4cf2bdef0719f445
arjav0703 <arjavjain0703@gmail.com>
2025-03-13 20:13:24 +0530

    add Top component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "31f1acdf9914f72d"; }
+/******/ 		__webpack_require__.h = function() { return "53ecdbe7dd6d5fab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ import {
   import Icon from '@hackclub/icons'
   import Flag from '../components/flag'
   import Team from '../components/team'
-
+  import Top from '../components/top'
 
   import React from 'react'
   // Next.js allows fast development of static sites with server-side rendering
@@ -79,35 +79,7 @@ const Super = styled(Text)`
           manifest="/site.webmanifest" />
       </Head>
       
-      <Box as="header" sx={{ bg: 'sunken', color: 'text' }}>
-        <Container sx={{ pt: 5, pb: [3, 4], textAlign: 'center' }}>
-        <Link href='https://hackclub.com'>
-          <img src="https://assets.hackclub.com/flag-orpheus-left.svg" alt="Hack Club Flag" style={{ position: 'fixed', top: 0, left: 0, width: '256px' }} />
-        </Link>
-          <ColorSwitcher />
-          <Heading as="h1" variant="title" color="red">
-          Hack Club Vidisha
-          </Heading>
-          <Grid
-            gap={4}
-            columns="auto auto auto"
-            sx={{
-              alignItems: 'center',
-              justifyContent: 'center',
-              fontSize: 2,
-              mt: 3,
-              mb: 4,
-              a: { color: 'muted', transition: 'color .125s ease-in-out' }
-            }}
-          > 
-            <Button as="a" href="https://github.com/orgs/HackClub-Vidisha/repositories" variant="outline">GitHub</Button>
-            <Button as="a" href="https://hack.club/0z65uj" variant="outline">Discord</Button>
-            <Button as="a" href="https://instagram.com/hackclubvidisha" variant="outline">Instagram</Button>
-          </Grid>
-        </Container>
-      </Box>
-
-
+      
       
       <Container sx={{my: 5}} variant='wide'>
         <Flex sx={{alignItems: 'center', flexWrap: 'wrap'}}>
